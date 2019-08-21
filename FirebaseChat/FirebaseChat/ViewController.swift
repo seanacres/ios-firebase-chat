e1c97e814d36650721ae71664205764d2b31fd3f
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-21 15:38:42 -0400

    Set up initial project and libraries with cocoapods

---
+++ FirebaseChat/FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Sean Acres on 8/21/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
